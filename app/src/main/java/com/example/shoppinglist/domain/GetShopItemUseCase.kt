6f06886e8b131656b0394ac0d3bb99aec67c56c6
alltime-ok <igor.duda6666@gmail.com>
2023-07-16 02:02:03 +0300

    creating domain file finish

--- app/src/main/java/com/example/shoppinglist/domain/GetShopItemUseCase.kt
+++ app/src/main/java/com/example/shoppinglist/domain/GetShopItemUseCase.kt
@@ -1,7 +1,7 @@
 package com.example.shoppinglist.domain
 
-class GetShopItemUseCase {
+class GetShopItemUseCase(private val shopListRepository: ShopListRepository) {
     fun getShopItem(shoItemId: Int): ShopItem{
-       TODO()
+       return shopListRepository.getShopItem(shoItemId)
     }
 }
---
+++ app/src/main/java/com/example/shoppinglist/domain/ShopListRepository.kt
@@ -0,0 +1,13 @@
+package com.example.shoppinglist.domain
+
+interface ShopListRepository {
+    fun addShopItem(shopItem: ShopItem)
+
+    fun deleteShopItem(shopItem: ShopItem)
+
+    fun editShopItem(shopItem: ShopItem)
+
+    fun getShopItem(shoItemId: Int): ShopItem
+
+    fun getShopList(): List<ShopItem>
+}
